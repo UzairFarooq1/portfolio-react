799836aeb23019c2fb18b8825fcc146454229175
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2024-01-29 14:25:58 +0300

    feat: :sparkles: home complete

---
+++ React-Portfolio/src/components/Home.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import Typed from 'typed.js';
+import profileImage from '../assets/images/profiledp.jpg';  // Import the image
+
+
+
+//styling
+const HomeContainer = styled.section`
+margin-top: 50px;
+text-align: center;
+padding: 2em;
+`;
+
+const Dp = styled.div`
+
+`
+const Image = styled.img`
+width: 300px;
+border-radius: 20%;
+height: 300px;
+border-style: double;
+border-color: black;
+border-width: 10px;
+`
+const StyledHr = styled.hr`
+border: 0;
+margin: 0 auto;
+height: 1px;
+background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));
+
+`
+
+const Info = styled.div`
+max-width: 700px;
+margin: 0 auto;
+vertical-align: middle;
+font-size: 16px;
+line-height: 1.6;
+`
+
+
+
+const Home = () =>{
+
+    useEffect(() => {
+        var talents = [
+          "A passionate Web Developer",
+          "An experienced Python developer",
+          "Skilled in Machine Learning and AI"
+        ];
+    
+        var options = {
+          strings: talents,
+          typeSpeed: 50,
+          backSpeed: 20,
+          loop: true,
+        };
+    
+        var typedOutput = document.getElementById('talent');
+
+        if (typedOutput) {
+          var typed = new Typed(typedOutput, options);
+        }
+    
+        // Clean up the Typed instance when the component unmounts
+        return () => {
+          if (typedOutput && typed) {
+            typed.destroy();
+          }
+        };
+      }, []); 
+  return (
+    <><HomeContainer>
+          <Dp>
+              <Image src={profileImage}  alt="profile" />
+          </Dp>
+          <h1>Hi there!</h1>
+          <span id="talent"></span>
+          <Info>
+              <p>Welcome To My Personal Portfolio! I Am A Passionate And Dedicated University Student Pursuing A Degree In BBIT.
+                  With A Strong Interest In Technology And Its Transformative Potential, I Am Constantly Seeking Opportunities
+                  To Expand My Knowledge And Skills In This Dynamic Field. Throughout My Academic Journey, I Have Gained
+                  A Solid Foundation In Areas Such As Programming Languages, Database Management And Network Fundamentals</p>
+          </Info>
+      </HomeContainer>
+      <StyledHr /></>
+
+  );
+}
+
+export default Home;
